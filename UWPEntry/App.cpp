c24faf02a34563af7b2088e02d98a1220c0c026a
Ethan McTague <ethan@tague.me>
2020-04-13 22:23:30 -0400

    Add UWP stuff

---
+++ UWPEntry/App.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "App.h"
+
+#include <ppltasks.h>
+
+using namespace UWPEntry;
+
+using namespace concurrency;
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Input;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+
+using Microsoft::WRL::ComPtr;
+
+// The DirectX 12 Application template is documented at https://go.microsoft.com/fwlink/?LinkID=613670&clcid=0x409
+
+// The main function is only used to initialize our IFrameworkView class.
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)
+{
+	auto direct3DApplicationSource = ref new Direct3DApplicationSource();
+	CoreApplication::Run(direct3DApplicationSource);
+	return 0;
+}
